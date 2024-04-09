279686d8059de73ca3fe20463ffebb9574aeb802
AmrAdel-Dev <166554196+AmrAdel-Dev@users.noreply.github.com>
2024-04-09 22:01:31 +0200

    Add files via upload

---
+++ JavaScript-Codes.js
@@ -0,0 +1,8 @@
+function remove_AC1_1(){
+    document.getElementById('AC1p').style.marginRight = '-650px';
+    document.getElementById('AC1').style.marginRight = '0px';
+}
+function remove_AC1_2(){
+    document.getElementById('AC1').style.marginRight = '-600px';
+    document.getElementById('AC1p').style.marginRight = '0px';
+}
